27df132dc1ea9a60459028568a29114e98e71144
kalab-scorpio <kalabkassa0@gmail.com>
2023-05-18 23:05:10 +0300

    trying to make the graph work

--- backend/backend/edge/views.py
+++ backend/backend/edge/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Vitals
 
-# Create your views here.
+heartRate = []
+temp = []
+time = []
+
 def main(request):
     vitals = Vitals.objects.all()
-    print(vitals)
-    return render(request, 'edge/main.html')
+    for vital in vitals:
+        heartRate.append(vital.heartRate)
+        temp.append(vital.temp)
+    i = 0
+    while(i < 60):
+        time.append(i)
+        i+1
+    return render(request, 'edge/main.html', {'heartRate':heartRate, 'temp': temp, 'time': time})
