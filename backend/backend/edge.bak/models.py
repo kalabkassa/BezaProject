7d82952aab711fe68a760d4463b309ea50c91d3a
kalab-scorpio <kalabkassa0@gmail.com>
2023-05-18 20:35:55 +0300

    database setup

--- backend/backend/edge.bak/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Vitals(models.Model):
-    userID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    heartRate = models.IntegerField(null=True)
-    temp = models.IntegerField(null=True)
-    spo2 = models.IntegerField(null=True)
-    timestamp = models.CharField(default='nan')
