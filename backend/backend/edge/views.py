7b6bcf48ffff3f6a849636579c97e4eff26ee53a
kalab-scorpio <kalabkassa0@gmail.com>
2023-05-18 21:09:14 +0300

    nothing good just a test

--- backend/backend/edge/views.py
+++ backend/backend/edge/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Vitals
 
 # Create your views here.
 def main(request):
+    query = Vitals.objects.create(heartRate=98, temp= 38,timestamp="May 2023 17 05:00")
+    query.save()
     return render(request, 'edge/main.html')
